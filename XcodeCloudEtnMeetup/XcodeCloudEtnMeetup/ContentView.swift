00d77f5bf56ec1bc5e2415f1729ebbd7140e42fe
Adam Pečev <adam.pecev@etnetera.cz>
2022-01-23 18:56:56 +0100

    initial setip

---
+++ XcodeCloudEtnMeetup/XcodeCloudEtnMeetup/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XcodeCloudEtnMeetup
+//
+//  Created by Adam Pečev on 23.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Xcode Cloud alive!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XcodeCloudEtnMeetup/XcodeCloudEtnMeetup/XcodeCloudEtnMeetupApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudEtnMeetupApp.swift
+//  XcodeCloudEtnMeetup
+//
+//  Created by Adam Pečev on 23.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCloudEtnMeetupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
